7eb60db0902d6189500d956ffbb20f2f8e83eac8
AngelMApps <angel456mora123@gmail.com>
2021-08-28 15:05:44 -0500

    2er d-28

--- src/pages/meetings/devocional.svelte
+++ src/pages/meetings/devocional.svelte
@@ -2,7 +2,7 @@
     import CompParallax from '../../components/parallax_comp.svelte' 
 </script>
 
-<CompParallax iURL='../images/cDevo.png'sHeight=39.9 />
+<CompParallax iURL='../images/cDevo.png' />
 
 
 <style>
